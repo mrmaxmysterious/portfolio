e72901e309ec393b1cf28c57fc8d6c222b685741
mrmaxmysterious <maxhenson28@icloud.com>
2021-12-31 13:48:09 +0000

    ✨ New Meta Info

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,18 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'max-portfolio',
+    title: 'Welcome!',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Hey! Check out my Portfolio here!' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/mainpfp.png' }
     ]
   },
 
